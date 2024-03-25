4c94f89a500123c1d381d4436c8154430ade2867
Roma <nikitinroma2605@gmail.com>
2024-03-25 16:45:00 +0200

    [refactor] refactored app.module.imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptorService} from "./core/interceptors/auth.interceptor.service";
-import {LoginComponent} from './login/login.component';
 import {AppComponentsModule} from "./shared/modules/app-components/app-components.module";
-import {AppMaterialModule} from "./shared/modules/app-material/app-material.module";
 import {ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        LoginComponent,
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
         HttpClientModule,
+        AppRoutingModule,
         AppComponentsModule,
-        AppMaterialModule,
         ReactiveFormsModule
     ],
     providers: [
