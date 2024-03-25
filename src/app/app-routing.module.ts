a69b8bf1bb04cdcf3c198dba1f2b57cff71f2a1c
Roma <nikitinroma2605@gmail.com>
2024-03-25 16:46:49 +0200

    [fix] fixed imports to app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./login/login.component";
 import {AdminMainComponent} from "./admin-main/admin-main.component";
 import {AdminMainModule} from "./admin-main/admin-main.module";
+import {TestsListComponent} from "./admin-main/tests-list/tests-list.component";
+import {UsersListComponent} from "./admin-main/users-list/users-list.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {EditUserItemComponent} from "./admin-main/edit-user-item/edit-user-item.component";
+import {UserMainComponent} from "./user-main/user-main.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+import {TestsToPassListComponent} from "./user-main/tests-to-pass-list/tests-to-pass-list.component";
+import {StartedTestsListComponent} from "./user-main/started-tests-list/started-tests-list.component";
 
 const routes: Routes = [
     {
@@ -10,6 +17,10 @@ const routes: Routes = [
         redirectTo: '/user-main',
         pathMatch: 'full'
     },
+    // {
+    //     path: 'user-main',
+    //     loadChildren: () => import('./user-main/user-main.module').then(m => m.UserMainModule)
+    // },
     {
         path: 'user-main',
         component: UserMainComponent,
