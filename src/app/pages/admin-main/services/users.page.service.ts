bda1c64ddee2334b282ea4bbfa5bec1c10846b43
Roma <nikitinroma2605@gmail.com>
2024-04-02 21:30:38 +0300

    [feat] added services for admin-main users-list, refactored code to use subjects, and observables for getting data, changing filters and paging

---
+++ src/app/pages/admin-main/services/users.page.service.ts
@@ -0,0 +1,103 @@
+import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
+import {Injectable} from "@angular/core";
+import {BehaviorSubject, catchError, combineLatest, Observable, of, switchMap, tap} from "rxjs";
+import {Filters} from "../../../core/interfaces/filters/filters";
+import {PagingSettings} from "../../../core/interfaces/paging-settings";
+import {PagedListModel} from "../../../core/interfaces/paged-list.model";
+import {UserApiService} from "../../../core/services/api/user.api.service";
+import {UserModel} from "../../../core/interfaces/user/user.model";
+
+@UntilDestroy()
+@Injectable()
+export class UsersPageService {
+    private filtersSubject: BehaviorSubject<Filters> = new BehaviorSubject<Filters>({
+        searchTerm: '',
+        sortColumn: '',
+        sortOrder: '',
+        selectFilters: {},
+    });
+
+    private pagingSettingSubject: BehaviorSubject<PagingSettings> = new BehaviorSubject<PagingSettings>({
+        page: 1,
+        pageSize: 3
+    });
+
+    private pagedListSubject: BehaviorSubject<PagedListModel<UserModel> | null> =
+        new BehaviorSubject<PagedListModel<UserModel> | null>(null);
+    private fetchingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+    public filters$: Observable<Filters> = this.filtersSubject.asObservable();
+    public pagingSetting$: Observable<PagingSettings> = this.pagingSettingSubject.asObservable();
+    public pagedListOfUsers$: Observable<PagedListModel<UserModel> | null> = this.pagedListSubject.asObservable();
+    public fetching$: Observable<boolean> = this.fetchingSubject.asObservable();
+
+    constructor(private userApiService: UserApiService) {
+        this.onFiltersAndPagingChange();
+    }
+
+    updateFilters(newFilters: Filters) {
+        this.resetPagingSettings();
+        this.filtersSubject.next(newFilters);
+    }
+
+    updatePagingSetting(newPagingSettings: PagingSettings) {
+        this.pagingSettingSubject.next(newPagingSettings);
+    }
+
+    private resetPagingSettings() {
+        this.pagingSettingSubject.next({
+            page: 1,
+            pageSize: 3
+        });
+    }
+
+    deleteUser(testId: string) {
+        this.userApiService
+            .deleteUser(testId)
+            .pipe(
+                untilDestroyed(this),
+                tap(() => {
+                    this.refreshUsers();
+                }),
+                catchError((error) => {
+                    console.error('Error deleting test:', error);
+                    return of(null);
+                }),
+            )
+            .subscribe();
+    }
+
+    private onFiltersAndPagingChange() {
+        combineLatest([this.filters$, this.pagingSetting$])
+            .pipe(
+                untilDestroyed(this),
+                tap(() => this.fetchingSubject.next(true)),
+                switchMap(([filters, pagedListSettings]) => {
+                        return this.loadTestsList$(filters, pagedListSettings)
+                    }
+                ),
+                tap(() => this.fetchingSubject.next(false))
+            )
+            .subscribe();
+    }
+
+    private loadTestsList$(filters: Filters, pagedListSettings: PagingSettings) {
+        return this.userApiService
+            .getAllUsers(pagedListSettings, filters)
+            .pipe(
+                tap((pagedList) => {
+                    this.pagedListSubject.next(pagedList);
+                }),
+                catchError((error) => {
+                    this.pagedListSubject.next(null);
+                    return of(null);
+                })
+            );
+    }
+
+    private refreshUsers() {
+        const currentFilters = this.filtersSubject.value;
+        const currentPagedListSettings = this.pagingSettingSubject.value;
+        this.loadTestsList$(currentFilters, currentPagedListSettings).pipe(untilDestroyed(this)).subscribe();
+    }
+}
--- src/app/pages/admin-main/users-list/users-list.component.ts
+++ src/app/pages/admin-main/users-list/users-list.component.ts
@@ -14,11 +14,16 @@ import {
 import {DialogDataDto} from "../../../core/interfaces/dialog/dialog-data.dto";
 import {ConfirmationDialogComponent} from "../../../shared/components/dialog/confirmation-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {UsersPageService} from "../services/users.page.service";
+import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
+import {filter} from "rxjs";
 
+@UntilDestroy()
 @Component({
     selector: 'app-users-list',
     templateUrl: './users-list.component.html',
-    styleUrls: ['./users-list.component.scss']
+    styleUrls: ['./users-list.component.scss'],
+    providers: [UsersPageService]
 })
 export class UsersListComponent {
     pagedListOfUsers: PagedListModel<UserModel> | null = null;
@@ -28,39 +33,24 @@ export class UsersListComponent {
     selectFilters: SelectFilter[] = Array.of(USER_ROLE_FILTER, EMAIL_CONFIRMED_FILTER);
     sortCriterias: SortCriteria[] = Array.of(CREATION_DATE_SORT_CRITERIA, MODIFICATION_DATE_SORT_CRITERIA);
 
-    pagingSettings: PagingSettings = {
-        page: 1,
-        pageSize: 3
-    }
-
-    filters: Filters = {
-        searchTerm: '',
-        sortColumn: '',
-        sortOrder: '',
-        selectFilters: {},
-    }
-
-    constructor(private usersService: UserApiService,
+    constructor(private usersPageService: UsersPageService,
                 private dialog: MatDialog) {
     }
 
     ngOnInit(): void {
+        this.usersPageService.fetching$.pipe(untilDestroyed(this)).subscribe(
+            value => this.isFetching = value
+        )
         this.loadUsers();
     }
 
     loadUsers(): void {
-        this.isFetching = true;
-        this.usersService.getAllUsers(this.pagingSettings, this.filters)
-            .subscribe(responseData => {
-                    console.log("success get request");
-                    this.pagedListOfUsers = responseData;
-                    this.users = responseData.items;
-                    this.isFetching = false;
-                },
-                error => {
-                    this.pagedListOfUsers = null;
-                    this.users = [];
-                })
+        this.usersPageService.pagedListOfUsers$.pipe(untilDestroyed(this)).subscribe(
+            response => {
+                this.pagedListOfUsers = response;
+                this.users = response?.items || [];
+            }
+        )
     }
 
     onDeleteUser(userId: string) {
@@ -72,30 +62,20 @@ export class UsersListComponent {
             data: dialogData
         });
 
-        dialogRef.afterClosed().subscribe((result) => {
-            if (result) {
-                this.usersService.deleteUser(userId)
-                    .subscribe(response => {
-                        this.users = this.users.filter(user => user.id !== userId);
-                        console.log(response);
-                    }, error => {
-                        console.log(error)
-                    })
-            }
-        });
+        dialogRef
+            .afterClosed()
+            .pipe(
+                untilDestroyed(this),
+                filter((result) => result),
+            )
+            .subscribe(() => this.usersPageService.deleteUser(userId));
     }
 
     onPageChangedEvent(pagingSetting: PagingSettings) {
-        this.pagingSettings = pagingSetting;
-        this.loadUsers();
+        this.usersPageService.updatePagingSetting(pagingSetting);
     }
 
     onFilterChange(filters: Filters) {
-        this.filters = filters;
-        this.pagingSettings = {
-            page: 1,
-            pageSize: 3
-        }
-        this.loadUsers();
+        this.usersPageService.updateFilters(filters);
     }
 }
