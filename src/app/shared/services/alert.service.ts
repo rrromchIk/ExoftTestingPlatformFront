84eccea19309b99f618627573eaeaa631995062c
Roma <nikitinroma2605@gmail.com>
2024-04-03 20:35:31 +0300

    [refactor] added keepAfterNavigationChange property, to have possibility to close snackbar if needed when navigating to another page

--- src/app/shared/services/alert.service.ts
+++ src/app/shared/services/alert.service.ts
@@ -1,18 +1,36 @@
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSnackBar, MatSnackBarRef, TextOnlySnackBar} from "@angular/material/snack-bar";
 import {Injectable} from "@angular/core";
+import {NavigationStart, Router} from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AlertService {
-    constructor(private snackBar: MatSnackBar) {
+    private keepAfterNavigationChange = true;
+    private currentSnackBar: MatSnackBarRef<TextOnlySnackBar> | null = null;
+
+    constructor(private snackBar: MatSnackBar, private router: Router) {
+        router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                if (this.keepAfterNavigationChange) {
+
+                    // only keep for many location changes
+                    this.keepAfterNavigationChange = true;
+                } else {
+                    if(this.currentSnackBar)
+                        this.currentSnackBar.dismiss();
+                }
+            }
+        });
     }
 
-    success(message: string) {
-        this.snackBar.open(message, 'Close', { duration: 8000, verticalPosition: 'bottom', panelClass: ['snackbar-success'] });
+    success(message: string, keepAfterNavigationChange = true) {
+        this.keepAfterNavigationChange = keepAfterNavigationChange;
+        this.currentSnackBar = this.snackBar.open(message, 'Close', { duration: 6000, verticalPosition: 'bottom', panelClass: ['snackbar-success'] });
     }
 
-    error(message: string) {
-        this.snackBar.open(message, 'Close', { duration: 8000, verticalPosition: 'bottom', panelClass: ['snackbar-error'] });
+    error(message: string, keepAfterNavigationChange = true) {
+        this.keepAfterNavigationChange = keepAfterNavigationChange;
+        this.currentSnackBar = this.snackBar.open(message, 'Close', { duration: 6000, verticalPosition: 'bottom', panelClass: ['snackbar-error'] });
     }
 }
