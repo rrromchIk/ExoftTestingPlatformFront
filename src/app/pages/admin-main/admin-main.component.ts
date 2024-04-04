3ca0b36911ef95d6f2bf08fec8d7196c6744ac32
Roma <nikitinroma2605@gmail.com>
2024-04-04 14:30:25 +0300

    [refactor] added register user link to navbar for superAdmin

--- src/app/pages/admin-main/admin-main.component.ts
+++ src/app/pages/admin-main/admin-main.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {AuthService} from "../../shared/services/auth.service";
+import {UserModel} from "../../core/interfaces/user/user.model";
 
 @Component({
     selector: 'app-admin-main',
@@ -6,4 +8,8 @@ import {Component} from '@angular/core';
     styleUrls: ['./admin-main.component.scss']
 })
 export class AdminMainComponent {
+    currentUser: UserModel | null;
+    constructor(authService: AuthService) {
+        this.currentUser = authService.getCurrentUser();
+    }
 }
