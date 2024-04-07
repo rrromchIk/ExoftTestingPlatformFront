b1cad4b5d5974dd7ff2be04a35c780bbbfd157cd
Roma <nikitinroma2605@gmail.com>
2024-04-08 00:41:49 +0300

    [feat] added userAnswer api service to create userAnswer

---
+++ src/app/core/services/api/user-answer.api.service.ts
@@ -0,0 +1,17 @@
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+})
+export class UserAnswerApiService {
+    private userAnswersEndpoint: string = `${environment.apiUrl}/api/users/answers`;
+
+    constructor(private http: HttpClient) {
+    }
+
+    createUserAnswer(userId: string, answerId: string, questionId: string) {
+        return this.http.post(this.userAnswersEndpoint, {userId, answerId, questionId});
+    }
+}
