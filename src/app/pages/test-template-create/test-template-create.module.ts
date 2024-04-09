f866cf24fb048bc6e5f89d3c3f754457958dd084
Roma <nikitinroma2605@gmail.com>
2024-04-09 21:06:11 +0300

    [refactor] imported AppMaterialModule

--- src/app/pages/test-template-create/test-template-create.module.ts
+++ src/app/pages/test-template-create/test-template-create.module.ts
@@ -2,14 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TestTemplateCreateComponent} from './test-template-create.component';
 import {AppComponentsModule} from "../../shared/modules/app-components/app-components.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatError, MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
-import {MatIcon} from "@angular/material/icon";
-import {MatInput} from "@angular/material/input";
-import {MatOption} from "@angular/material/autocomplete";
-import {MatSelect} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
+
+import {AppMaterialModule} from "../../shared/modules/app-material/app-material.module";
 
 
 @NgModule({
@@ -19,21 +14,7 @@ import {MatSelect} from "@angular/material/select";
     imports: [
         CommonModule,
         AppComponentsModule,
-        FormsModule,
-        MatButton,
-        MatCard,
-        MatCardContent,
-        MatCardHeader,
-        MatCardTitle,
-        MatError,
-        MatFormField,
-        MatIcon,
-        MatIconButton,
-        MatInput,
-        MatLabel,
-        MatOption,
-        MatSelect,
-        MatSuffix,
+        AppMaterialModule,
         ReactiveFormsModule
     ]
 })
