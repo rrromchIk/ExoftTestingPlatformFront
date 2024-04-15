cebdd21aba650be59ee69d35ce312b662a571f0e
Roma <nikitinroma2605@gmail.com>
2024-04-15 12:44:24 +0300

    [feat] added totalTimeSpent for userStatistic

--- src/app/core/interfaces/statistic/user-statistic.model.ts
+++ src/app/core/interfaces/statistic/user-statistic.model.ts
@@ -2,6 +2,7 @@ export interface UserStatisticModel {
     amountOfStartedTests: number;
     amountOfCompletedTests: number;
     amountOfInProcessTests: number;
+    totalTimeSpentInMinutes: number;
     averageResult?: number;
     allTestsResults: number[];
 }
